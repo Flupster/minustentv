d193becf42c913225598b1059f5b64618a8202df
Flupster <kyle.henfrey@gmail.com>
2020-09-09 18:12:22 +0000

    stream schema userId changed to ObjectId

--- models/Stream.js
+++ models/Stream.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const StreamSchema = new mongoose.Schema({
   inputType: String,
   inputSource: String,
-  userId: String,
+  userId: mongoose.Types.ObjectId,
   date: { type: Date, default: Date.now },
 });
 

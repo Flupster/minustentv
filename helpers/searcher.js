46956f02f5e27aeb2aa1958bbb63ca1774f499ce
Flupster <kyle.henfrey@gmail.com>
2021-02-08 18:55:16 +0000

    Update Meta when new file detected
    This will make the update-media cli tool useless as all meta will be applied on startup AND when new files are added

--- helpers/searcher.js
+++ helpers/searcher.js
@@ -1,11 +1,13 @@
 const Fuse = require("fuse.js");
 const chokidar = require("chokidar");
+const Media = require("../models/Media");
+const Meta = require("./meta");
 
 class Searcher {
   constructor() {
     this.files = [];
     this.watcher = chokidar.watch(process.env.MEDIA_GLOB);
-    this.watcher.on("add", file => this.files.push(file));
+    this.watcher.on("add", this.onNewFile.bind(this));
     this.watcher.on("ready", () => console.log("Searcher is ready..."));
   }
 
@@ -13,6 +15,23 @@ class Searcher {
     const fuse = new Fuse(this.files);
     const search = fuse.search(term).splice(0, 100);
     return search.map(x => x.item);
+  }
+
+  async onNewFile(file) {
+    this.files.push(file);
+
+    const media = await Media.findByFile(file);
+
+    if (!media) {
+      const meta = await Meta.getMeta(file);
+      await Media.createOrUpdate({ file }, meta);
+
+      if (meta.scene.type === "tvshow") {
+        console.log("Matched TV:", file, "=>", meta.tmdb?.name);
+      } else if (meta.scene.type === "movie") {
+        console.log("Matched Movie:", file, "=>", meta.tmdb?.title);
+      }
+    }
   }
 }
 

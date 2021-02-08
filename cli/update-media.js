a2b7af5a4137b8ff2a91cb6fd2bbcd8b0dae1532
Flupster <kyle.henfrey@gmail.com>
2021-02-08 18:33:04 +0000

    update media updater to use searcher

--- cli/update-media.js
+++ cli/update-media.js
@@ -8,45 +8,44 @@ const Movie = require("../models/Movie");
 const TvShow = require("../models/TvShow");
 
 const Meta = require("../helpers/meta");
-const rrdir = require("rrdir");
+const Searcher = require("../helpers/searcher");
 const cliProgress = require("cli-progress");
 
-mongoose
-  .connect(process.env.DB_CONNECT, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(start)
-  .catch(e => {
-    console.error(e);
-    process.exit(1);
-  });
+Searcher.watcher.on("ready", () => {
+  mongoose
+    .connect(process.env.DB_CONNECT, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(start)
+    .catch(e => {
+      console.error(e);
+      process.exit(1);
+    });
+});
 
 async function start() {
-  const files = await rrdir.async(process.env.MEDIA_DIR, {
-    exclude: ["**/.*"],
-    include: [process.env.MEDIA_GLOB],
-  });
+  const files = Searcher.files;
 
   const bar = new cliProgress.SingleBar({ hideCursor: true }, cliProgress.Presets.shades_classic);
   bar.start(files.length, 0);
 
   for (const file of files) {
-    const doc = await Media.findByFile(file.path);
+    const doc = await Media.findByFile(file);
 
     // New file
     if (!doc) {
-      const meta = await Meta.getMeta(file.path);
-      const media = await Media.createOrUpdate({ file: file.path }, meta);
+      const meta = await Meta.getMeta(file);
+      const media = await Media.createOrUpdate({ file }, meta);
       if (media.tmdb) {
         media.isMovie() ? Movie.updateMovie(media.tmdb.id) : TvShow.updateTvShow(media.tmdb.id);
       }
     } else {
       // File updated
       if (await doc.hasChanged()) {
-        const meta = await Meta.getMeta(file.path);
-        Media.createOrUpdate({ file: file.path }, meta);
+        const meta = await Meta.getMeta(file);
+        Media.createOrUpdate({ file }, meta);
       }
 
       // Update meta from TMDB
